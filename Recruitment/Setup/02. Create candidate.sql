e1ddaf88fb19c743e0598c8e2fbaaf4fc3c597fb
Nick Allan <nickallan.only@gmail.com>
2024-02-04 00:08:08 +0000

    Removal of chaff

--- Recruitment/Setup/02. Create candidate.sql
+++
@@ -1,6 +0,0 @@
---Run from DB(s) user requires access to.  E.g. diligencia-t
-DROP USER IF EXISTS [candidate];
-GO
-CREATE USER [candidate] WITH PASSWORD = 'Int3rv13w';
-
-ALTER ROLE db_owner ADD MEMBER [candidate];
